7d55557780e5e4742cf976f2c136e8e7435fe531
Joseph Taylor <joseph.ross.taylor@cern.ch>
2016-04-23 11:50:02 +0100

    added turnOn curve plotter

--- makePlots.cxx
+++ makePlots.cxx
@@ -1,19 +1,30 @@
 #include "TFile.h"
+#include "TF1.h"
 #include "TH1F.h"
 #include "TH2F.h"
 #include "TStyle.h"
 #include "TLegend.h"
+#include "TEfficiency.h"
 #include <TCanvas.h>
 #include <TLatex.h>
 #include <string>
 #include <iostream>
 
+//TODO: insert error message explanations/terminations to avoid seg faults
+
 class rootPlotMaker{
 //private class members: only accessible from within other members of this class
 	vector<TFile*> inputFileObjects;
     TFile * inputFile2D;
+    TFile * inputFileEff;
 	vector<TH1F*> histogramObjects;
-    TH2F * histogram2D;	    	
+    TH2F * histogram2D;	
+    vector<TEfficiency> efficiencyObjects;
+
+    vector<TF1*> turnOnFits;
+
+    vector<TH1F*> numeratorObjects;
+    vector<TH1F*> denominatorObjects;    	
 	TCanvas * canvas = new TCanvas("canvas", "", 650, 600);
 	TLegend legend;
 	TLatex additionalText;
@@ -21,7 +32,7 @@ public:
 //public class members: can be individually accessed from outside the class
 	void loadHistogramsInfo(vector<string>,vector<string>,vector<int>,vector<int>,vector<int>);
 	void loadHistogram2D(string,string);
-	void loadEfficiencies();
+	void loadEfficiencies(string,vector<string>,vector<string>,vector<string>,vector<int>,vector<int>,vector<int>);
 	void plotAndSave(int,string,string);
 	// extension functions you can run (after loading and before plotting)...not complete, add as you need
 	void turnGridLinesOn();
@@ -46,7 +57,7 @@ void makePlots(){
 
 	// set unique parameters
 	// set up to primary operate on a single root file
-	string directoryName = "output_rates/run269224_zeroBias_v34p0/"; //also the directory where we save final plots
+	string directoryName = "output_jets/run269224_zeroBias_v34p0/"; //also the directory where we save final plots
 	string secondFileDirectoryName = "";
 
 	string inputFileName = "histos.root"; // set up analysis macros to name output this
@@ -123,38 +134,70 @@ void makePlots(){
 	// key parameters to change:
 	// histogram names, legend names & position (if legend desired), pdf file name
 
-	rootPlotMaker plot_vx1;
-    vector<string> histoNames_vx1;
-    histoNames_vx1.push_back("etSumDist_emu");
-    histoNames_vx1.push_back("etSumDist_hw");
-    histoNames_vx1.push_back("htSumDist_emu");   
-    histoNames_vx1.push_back("htSumDist_hw");     
-	vector<string> legIconNames_vx1;
-	legIconNames_vx1.push_back("et_emu");
-	legIconNames_vx1.push_back("et_hw");
-	legIconNames_vx1.push_back("ht_emu");
-	legIconNames_vx1.push_back("ht_hw");
-	plot_vx1.loadHistogramsInfo(inputFilePath_allSame,histoNames_vx1,histogramLineWidths_allSize2,
-					   histogramLineColours_allDifferent,histogramLineStyles_allSolidLines);
-	plot_vx1.insertLegend(legIconNames_vx1, 0.65, 0.85, 0.65, 0.85);
-	plot_vx1.plotAndSave(1, directoryName.c_str(), "et_ht_emuHw_dist.pdf");
+
+	// rootPlotMaker plot_vx1;
+	// vector<string> histoNames_vx1;
+	// histoNames_vx1.push_back("etSumDist_emu");
+	// histoNames_vx1.push_back("etSumDist_hw");
+	// histoNames_vx1.push_back("htSumDist_emu");   
+	// histoNames_vx1.push_back("htSumDist_hw");     
+	// vector<string> legIconNames_vx1;
+	// legIconNames_vx1.push_back("et_emu");
+	// legIconNames_vx1.push_back("et_hw");
+	// legIconNames_vx1.push_back("ht_emu");
+	// legIconNames_vx1.push_back("ht_hw");
+	// plot_vx1.loadHistogramsInfo(inputFilePath_allSame,histoNames_vx1,histogramLineWidths_allSize2,
+	// 				   histogramLineColours_allDifferent,histogramLineStyles_allSolidLines);
+	// plot_vx1.insertLegend(legIconNames_vx1, 0.65, 0.85, 0.65, 0.85);
+	// plot_vx1.plotAndSave(1, directoryName.c_str(), "et_ht_emuHw_dist.pdf");
+
+	// rootPlotMaker plot_vx2;
+	// vector<string> histoNames_vx2;
+	// histoNames_vx2.push_back("etSumDist_emu");
+	// histoNames_vx2.push_back("etSumDist_hw");
+	// histoNames_vx2.push_back("htSumDist_emu");   
+	// histoNames_vx2.push_back("htSumDist_hw");     
+	// vector<string> legIconNames_vx2;
+	// legIconNames_vx2.push_back("et_emu");
+	// legIconNames_vx2.push_back("et_hw");
+	// legIconNames_vx2.push_back("ht_emu");
+	// legIconNames_vx2.push_back("ht_hw");
+	// plot_vx2.loadHistogramsInfo(inputFilePath_allSame,histoNames_vx2,histogramLineWidths_allSize2,
+	// 				   histogramLineColours_allDifferent,histogramLineStyles_allSolidLines);
+	// plot_vx2.insertLegend(legIconNames_vx2, 0.65, 0.85, 0.65, 0.85);
+	// plot_vx2.plotAndSave(1, directoryName.c_str(), "et_ht_emuHw_dist.pdf");
 
 
-	rootPlotMaker plot_vx2;
-    vector<string> histoNames_vx2;
-    histoNames_vx2.push_back("etSumDist_emu");
-    histoNames_vx2.push_back("etSumDist_hw");
-    histoNames_vx2.push_back("htSumDist_emu");   
-    histoNames_vx2.push_back("htSumDist_hw");     
-	vector<string> legIconNames_vx2;
-	legIconNames_vx2.push_back("et_emu");
-	legIconNames_vx2.push_back("et_hw");
-	legIconNames_vx2.push_back("ht_emu");
-	legIconNames_vx2.push_back("ht_hw");
-	plot_vx2.loadHistogramsInfo(inputFilePath_allSame,histoNames_vx2,histogramLineWidths_allSize2,
-					   histogramLineColours_allDifferent,histogramLineStyles_allSolidLines);
-	plot_vx2.insertLegend(legIconNames_vx2, 0.65, 0.85, 0.65, 0.85);
-	plot_vx2.plotAndSave(1, directoryName.c_str(), "et_ht_emuHw_dist.pdf");
+
+	rootPlotMaker plot_ve1;
+    vector<string> histoNums_ve1;
+    histoNums_ve1.push_back("hnum36_central");
+    histoNums_ve1.push_back("hnum52_central");
+    histoNums_ve1.push_back("hnum68_central");   
+    histoNums_ve1.push_back("hnum92_central"); 
+    histoNums_ve1.push_back("hnum128_central"); 
+    vector<string> histoDens_ve1;
+    histoDens_ve1.push_back("hden_central");
+    histoDens_ve1.push_back("hden_central");
+    histoDens_ve1.push_back("hden_central");   
+    histoDens_ve1.push_back("hden_central"); 
+    histoDens_ve1.push_back("hden_central"); 
+    vector<string> histoEffs_ve1;
+    histoEffs_ve1.push_back("hT36_central");
+    histoEffs_ve1.push_back("hT52_central");
+    histoEffs_ve1.push_back("hT68_central");   
+    histoEffs_ve1.push_back("hT92_central"); 
+    histoEffs_ve1.push_back("hT128_central"); 
+	vector<string> legIconNames_ve1;
+	legIconNames_ve1.push_back("36");
+	legIconNames_ve1.push_back("52");
+	legIconNames_ve1.push_back("68");
+	legIconNames_ve1.push_back("92");
+	legIconNames_ve1.push_back("128");
+	plot_ve1.loadEfficiencies(path,histoNums_ve1,histoDens_ve1,histoEffs_ve1,histogramLineWidths_allSize2,
+					     	   histogramLineColours_allDifferent,histogramLineStyles_allSolidLines);
+	plot_ve1.insertLegend(legIconNames_ve1, 0.65, 0.85, 0.65, 0.85);
+	plot_ve1.plotAndSave(3, directoryName.c_str(), "turnOnTrial.pdf");
 
 return;
 } // close makePlots function
@@ -194,26 +237,69 @@ void rootPlotMaker::loadHistogram2D(string inputFilename2D, string histogramName
 	return;
 }
 
-void rootPlotMaker::loadEfficiencies(){
-	/*
-pass it a vector of numerators
-pass it a vector of denominators
-create a vector of Teffs
-create a vector?? of fits
-	*/
+void rootPlotMaker::loadEfficiencies(string inputFilenameEffDummy, vector<string> histoNumeratorNames,
+									   vector<string> histoDenominatorNames, vector<string> histoEffNames, vector<int> histogramLineWidthsDummy,
+						   			   vector<int> histogramLineColoursDummy, vector<int> histogramLineStylesDummy){
+
+	for (unsigned int i=0; i<histoNumeratorNames.size(); i++){
+
+		inputFileEff = TFile::Open(inputFilenameEffDummy.c_str());
+
+		numeratorObjects.push_back((TH1F*)inputFileEff->Get(histoNumeratorNames[i].c_str()));
+		denominatorObjects.push_back((TH1F*)inputFileEff->Get(histoDenominatorNames[i].c_str()));
+
+		histogramObjects.push_back((TH1F*)inputFileEff->Get(histoEffNames[i].c_str())); // re-use so we can use the same manipulations of the plot
+		histogramObjects[i]->SetLineWidth(histogramLineWidthsDummy[i]);
+		histogramObjects[i]->SetLineColor(histogramLineColoursDummy[i]);
+		histogramObjects[i]->SetLineStyle(histogramLineStylesDummy[i]);		
+
+		efficiencyObjects.push_back(TEfficiency(*numeratorObjects[i],*denominatorObjects[i]));
+		efficiencyObjects[i].SetLineWidth(histogramLineWidthsDummy[i]);
+		efficiencyObjects[i].SetLineColor(histogramLineColoursDummy[i]);
+		efficiencyObjects[i].SetLineStyle(histogramLineStylesDummy[i]);
+
+		// TODO: put this stuff in...
+		// efficiencyObjects[i]->SetMarkerSize(histogramMarkerSizesDummy[i]);		
+		// efficiencyObjects[i]->SetMarkerColor(histogramMarkerColoursDummy[i]);
+		// efficiencyObjects[i]->SetMarkerStyle(histogramMarkerStylesDummy[i]);
+
+		// TODO: tune the input parameters better by investigating pre-fit
+		// TODO: fit to a user defined range using ~efficiencyObjects[i].Fit(turnOnFits[i], "R");	
+		TF1* errFunc = new TF1("errFunc","[0]*0.5*(TMath::Erf((x-[1])/[2])+1)",0,histogramObjects[i]->GetXaxis()->GetXmax());
+	    turnOnFits.push_back(errFunc);
+	    turnOnFits[i]->SetParameters(1.00, 40, 10);
+	    turnOnFits[i]->SetLineColor(histogramLineColoursDummy[i]);
+	    efficiencyObjects[i].Fit(turnOnFits[i], "R");
+	    // histogramObjects[i]->Fit(turnOnFits[i]);
+	}
+
+	histogramObjects[0]->GetXaxis()->SetTitleOffset(1.2);
+	histogramObjects[0]->GetYaxis()->SetTitleOffset(1.2);
+	gStyle->SetOptStat(0);
+	return;
 }
+	
+// plotType 1 = normal 1d; plotType 2 = 2d; plotType 3 = efficiencyPlot.	
+void rootPlotMaker::plotAndSave(int plotType, string saveDirName, string savePlotName){
 
-//TODO: to include Tefficiency soon...
-void rootPlotMaker::plotAndSave(int plotDimension, string saveDirName, string savePlotName){
-
-	if (plotDimension==1){
+	if (plotType==1){
 		for (unsigned int i=0; i<histogramObjects.size(); i++){
 			histogramObjects[i]->Draw("same");
 		}
 	}
 
-	if (plotDimension==2){
+	if (plotType==2){
 		histogram2D->Draw("cols, z");
+	}
+
+	if (plotType==3){
+		for (unsigned int i=0; i<efficiencyObjects.size(); i++){	
+			
+			if (i==0) histogramObjects[i]->Draw("P");
+			else histogramObjects[i]->Draw("same, P");
+			efficiencyObjects[i].GetListOfFunctions()->AddFirst(turnOnFits[i]);	
+			efficiencyObjects[i].Draw("same");
+		}
 	}
 
 	legend.Draw("same");
@@ -225,7 +311,7 @@ void rootPlotMaker::plotAndSave(int plotDimension, string saveDirName, string sa
  	return;
 }
 
-// extension functions you can run (after loading and before plotting)...not complete, add as you need
+// extension functions you can run (after loading and before plotting)...not complete, add as you need them
 void rootPlotMaker::turnGridLinesOn(){canvas->SetGrid(); return;}
 void rootPlotMaker::turnLogXon(){gPad->SetLogx(); return;}
 void rootPlotMaker::turnLogYon(){gPad->SetLogy(); return;}
@@ -263,7 +349,7 @@ void rootPlotMaker::setCanvasSize(double width, double height){
 	return;}
 
 void rootPlotMaker::insertLegend(vector<string> legendIconNames, double xmin, 
-				  double xmax, double ymin, double ymax){
+								  double xmax, double ymin, double ymax){
 
 	for (unsigned int i=0; i<histogramObjects.size(); i++){
 		legend.AddEntry(histogramObjects[i], legendIconNames[i].c_str(), "L");
@@ -306,14 +392,3 @@ void rootPlotMaker::setAdditionalTextSize(double additionalTextSize){
 	additionalText.SetTextSize(additionalTextSize);
 	return;
 }
-
-
-
-
-
-
-
-
-
-
-
